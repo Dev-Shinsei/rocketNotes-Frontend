a8ab683d6a144b34b41c87a6fd7ad58846afaa6f
Carlos Gomes <dev.shinsei@gmail.com>
2022-07-23 00:33:59 -0300

    bug on global.js fixed

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,11 @@ export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+        possimus quo beatae nulla in ea cupiditate libero, nisi, delectus esse
+        pariatur. Corrupti autem maiores rem magnam ipsam dicta hic quas?
+      </p>
     </Container>
   );
 }
