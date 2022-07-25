913650a0ca198441cf9d84364cc5e9574ee2b68a
Carlos Gomes <dev.shinsei@gmail.com>
2022-07-25 13:51:47 -0300

    Component Header Styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
-export function Header(){
-  return(
-    <Container />
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/Dev-Shinsei.png"
+          alt="Foto de Carlos Gomes"
+        />
 
-  )
-
+        <div>
+          <span>Bem Vindo</span>
+          <strong>Carlos Gomes</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -6,8 +6,6 @@ import { Button } from "../../components/Button";
 export function Details() {
   return (
     <Container>
-      <h1>Hello World</h1>
-      <p>Carlos Gomes</p>
       <Header />
 
       <Button title="Voltar" />
