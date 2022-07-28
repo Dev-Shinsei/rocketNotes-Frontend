29eb87f3515a3201e0b16d6abed5964a6027c6da
Carlos Gomes <dev.shinsei@gmail.com>
2022-07-28 00:16:32 -0300

    home page finalized

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,10 @@
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Section } from "../../components/Section";
+import { Note } from "../../components/Note";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
   return (
@@ -10,13 +15,43 @@ export function Home() {
 
       <Header />
 
-      <Menu></Menu>
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
+      </Menu>
 
-      <Search></Search>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+      </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas Notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                {
+                  id: "1",
+                  name: "React",
+                },
+                { id: "2", name: "Rocketseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
 
-      <NewNote></NewNote>
+      <NewNote>
+        <FiPlus />
+        Criar Nota
+      </NewNote>
     </Container>
   );
 }
